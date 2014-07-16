cd9a3c3aea6500dc20a6bf68b37a6142e8f357b1
linxuping <linxuping@126.com>
2014-07-16 21:48:34 +0800

    import

---
+++ django_composite/view.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+from django.template import Template,Context,RequestContext
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt  
+def visit_bootstrap(request):
+  fp = open('django_composite/bootstrap.html')  
+  t = Template(fp.read())  
+  fp.close()  
+  html = t.render(Context({"id":1}))  
+  return HttpResponse(html) 
+  
