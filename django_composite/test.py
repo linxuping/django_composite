e43a4f3f70d3cb55f51d2a0dc16d523efb8a77dd
linxuping <linxuping@126.com>
2015-11-25 23:05:37 +0800

    test cases

--- django_composite/test.py
+++ django_composite/test.py
@@ -3,24 +3,72 @@
 import urllib2
 from lxml import etree
 def get_nodes(_url, _xpath):
+  #headers = {'User-Agent':'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'}  
+  #req = urllib2.Request(url=_url,headers=headers)
   resp = urllib2.urlopen(_url)
   import time
-  print resp
+  #print resp
   res = resp.read()
+  #ret = res.find("22c9.png")
+  #if ret:
+  #  ret = ret - 200
+  #  print res[ret:ret+250]
+  f = open("test.html", "w")
+  f.write(res)
+  f.close()
   tree = etree.HTML(res)
-  print tree
   return tree.xpath(_xpath)
   
 #print len(get_nodes("http://tech.163.com/latest", '//*[@id="instantPanel"]/div[1]/ul/li[1]/a[2]'))
 #nodes = get_nodes("http://tech.163.com/", '//h2[@class="color-link"]/a')
 #nodes = get_nodes("http://tech.qq.com/", '//div[@class="Q-tpList"]/div/h3/a')
 #nodes = get_nodes("http://tech.sina.com.cn/", '//a[@target="_blank"]')
-nodes = get_nodes("http://3g.163.com/touch/sports/", '//p')
-print len(nodes)
-for node in nodes:
-  try:
-    print node.text,node.getparent().getparent().get("href")
-  except:
-    print "exception."
-    pass
+def check_img(url,xpaths,expect):
+	count = 0
+	for xpath in xpaths:
+		nodes = get_nodes(url,xpath)
+		for node in nodes:
+			try:
+				#print node.get("src")
+				count = count + 1
+			except:
+				print "exception."
+	if expect != count:
+		print url, "expect:%d, actual:%d "%(expect, count)
+
+
+
+xpaths=[]
+#check_img("", xpaths, 2)
+
+#img
+if False:
+	# 3g.news.cn cases. 
+	xpaths=['//p/img']
+	check_img("http://3g.news.cn//html/947/201651.html", xpaths, 1)
+	check_img("http://3g.news.cn/html/694/201398.html", xpaths, 0)
+	#check_img("http://3g.news.cn/html/113/201841.html", xpaths, 1)
+	check_img("http://3g.news.cn/html/158/201886.html", xpaths, 3)
+	check_img("http://3g.news.cn/html/122/201850.html", xpaths, 3)
+
+	#http://inews.ifeng.com/
+	xpaths=["//p/img"]
+	check_img("http://inews.ifeng.com/46385628/news.shtml", xpaths, 2)
+	check_img("http://inews.ifeng.com/46384592/news.shtml", xpaths, 1)
+	check_img("http://inews.ifeng.com/46385131/news.shtml", xpaths, 1)
+	check_img("http://inews.ifeng.com/46385131/news.shtml", xpaths, 1)
+	check_img("http://inews.ifeng.com/46385628/news.shtml", xpaths, 2)
+
+	# http://news.qq.com/  cannot
+
+	xpaths=["//div/img[@class='j_fullppt']","//div/img[@class='j_fullppt_cover']"]
+	check_img("http://news.sina.cn/sh/2015-11-25/detail-ifxkwuwv3597971.d.html?vt=4&pos=8&cid=56261", xpaths, 2)
+	check_img("http://news.sina.cn/gn/2015-11-25/detail-ifxkwuwy7112185.d.html?vt=4&pos=8&cid=56261", xpaths, 2)
+	check_img("http://news.sina.cn/sh/2015-11-25/detail-ifxkxfvn9008810.d.html?vt=4&pos=11&cid=56264", xpaths, 2)
+	check_img("http://news.sina.cn/sh/2015-11-25/detail-ifxkwuwv3601067.d.html?vt=4&pos=11&cid=56264", xpaths, 1)
+	check_img("http://news.sina.cn/sh/2015-11-25/detail-ifxkwuwy7117086.d.html?vt=4&pos=11&cid=56264", xpaths, 1)
+
+
+	print "test fin."
+
 
---
+++ django_composite/test2.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import urllib2
+from lxml import etree
+def get_nodes(_url, _xpath):
+  #headers = {'User-Agent':'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'}  
+  #req = urllib2.Request(url=_url,headers=headers)
+  resp = urllib2.urlopen(_url)
+  import time
+  #print resp
+  res = resp.read()
+  #ret = res.find("22c9.png")
+  #if ret:
+  #  ret = ret - 200
+  #  print res[ret:ret+250]
+  f = open("test.html", "w")
+  f.write(res)
+  f.close()
+  tree = etree.HTML(res)
+  return tree.xpath(_xpath)
+
+
+def get_href(node):
+	href = None
+	for i in range(6):
+		href = node.get("href")
+		if href != None:
+			return href
+		node = node.getparent()
+		if node == None:
+			return None
+  
+#print len(get_nodes("http://tech.163.com/latest", '//*[@id="instantPanel"]/div[1]/ul/li[1]/a[2]'))
+#nodes = get_nodes("http://tech.163.com/", '//h2[@class="color-link"]/a')
+#nodes = get_nodes("http://tech.qq.com/", '//div[@class="Q-tpList"]/div/h3/a')
+#nodes = get_nodes("http://tech.sina.com.cn/", '//a[@target="_blank"]')
+def analyze_new(url,xpaths):
+	count = 0
+	for xpath in xpaths:
+		nodes = get_nodes(url,xpath)
+		for node in nodes:
+			try:
+				_text = node.text
+				_href = get_href(node)
+				print _href
+				if not _text or not _href:
+					continue
+				print _text,_href
+				count = count + 1
+			except:
+				print "exception.",node, sys.exc_info()
+	print url, "count: %d"%count
+
+
+
+xpaths=["//li/a"]
+analyze_new("http://shehui.news.baidu.com/", xpaths)
+
+#img
+if False:
+	xpaths=["//div[@class='newlist']/ul/li/a"]
+	analyze_new("http://3g.news.cn/html/", xpaths)
+
+	xpaths=["//p"]
+	analyze_new("http://inews.ifeng.com/", xpaths)
+
+	xpaths=["//section/p/a","//h4/a/strong","//div/div/a"]
+	analyze_new("http://m.sohu.com/", xpaths)
+
+	xpaths=["//h3[@class='carditems_list_h3']"]
+	analyze_new("http://news.sina.cn/", xpaths)
+
+	xpaths=["//li/h4/a"]
+	analyze_new("http://news.163.com/mobile/", xpaths)
+
+	xpaths=["//h2"]
+	analyze_new("http://xw.qq.com/m/news", xpaths)
+
+	print "test fin."
+
+
